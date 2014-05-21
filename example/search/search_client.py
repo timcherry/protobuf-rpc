39dceebc9dcca1caa8f93088db9b3b568a55f52d
Tim Cherry <tcherry@livefyre.com>
2014-05-21 14:59:21 -0700

    simple TCP Server working

--- example/search/search_client.py
+++ example/search/search_client.py
@@ -1,13 +1,20 @@
 from SearchService_pb2 import SearchService_Stub, SearchService, SearchRequest
 from channel import SocketRpcChannel
 
-request = SearchRequest()
-request.query = "tim"
+
 
 channel = SocketRpcChannel(host='localhost', port=1234)
 service = SearchService_Stub(channel)
 controller = channel.newController()
 
-
+request = SearchRequest()
+request.query = "tim"
 SearchService_Stub.Search(service, controller, request)
+
+
+req = SearchRequest()
+req.query = "foopoo"
+SearchService_Stub.Search(service, controller, req)
+
+
 
--- example/search/search_server.py
+++ example/search/search_server.py
@@ -1,11 +1,22 @@
 from SearchService_pb2 import SearchService, SearchResponse
+from server import SocketRpcServer
 
 class SearchImpl(SearchService):
-    def Search(self, controller, request, done): 
+    def Search(self, controller, request, done):
         print "In Search!"
+
+        print "QUERY:", request.query
 
         response = SearchResponse()
         response.response = "booooya"
 
 
         done.run(response)
+
+
+
+srv = SocketRpcServer(1234)
+srv.registerService(SearchImpl())
+
+print "SERVING"
+srv.run()
