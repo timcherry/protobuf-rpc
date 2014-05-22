789e339c9aa3e28afc3d006e79da80e73664960d
Tim Cherry <tcherry@livefyre.com>
2014-05-22 13:55:49 -0700

    sock.reuseaddr

--- src/tcprpc/server.py
+++ src/tcprpc/server.py
@@ -5,6 +5,7 @@ from common.base_server import ProtoBufRPCServer
 class RawTCPServer(ProtoBufRPCServer):
     def __init__(self, host, port, service):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((host, port))
         self.service = service
 
