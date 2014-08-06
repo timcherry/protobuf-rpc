a9bb6dbeb468e2208f22f1bb6bc9a59448c16fa5
Tim Cherry <tcherry@livefyre.com>
2014-08-06 09:54:24 -0700

    reducing test complexity

--- tests/unit_test/test_connection.py
+++ tests/unit_test/test_connection.py
@@ -11,18 +11,14 @@ class TestConnection(unittest.TestCase):
 
     def setUp(self,):
         ctx1 = zmq.Context()
-        ctx2 = zmq.Context()
         self.serv_con1 = ctx1.socket(zmq.ROUTER)
-        self.serv_con2 = ctx2.socket(zmq.ROUTER)
         self.serv_con1.bind("tcp://127.0.0.1:12345")
-        self.serv_con2.bind("tcp://127.0.0.1:12346")
-        self.hosts = [("127.0.0.1",12345),("127.0.0.1", 12346)]
+        self.hosts = [("127.0.0.1",12345)]
         self.con = ZMQConnection(self.hosts)
 
     def tearDown(self):
-        self.serv_con1.close()
-        self.serv_con2.close()
         self.con.close()
+        self.serv_con1.close()
 
     def test_send(self):
         mock_msg = "PING"
@@ -30,8 +26,9 @@ class TestConnection(unittest.TestCase):
         msg1 = self.serv_con1.recv_multipart()
         self.assertEquals(msg1[2], mock_msg)
 
-
     def test_send_recv(self):
+        self.hosts = [("127.0.0.1",12345)]
+        self.con = ZMQConnection(self.hosts)
         mock_msg = "PING"
         self.con.send(mock_msg)
         [id_, null, req] = self.serv_con1.recv_multipart()
@@ -40,3 +37,4 @@ class TestConnection(unittest.TestCase):
         self.serv_con1.send_multipart([id_, null, mock_resp])
         resp = self.con.recv()
         self.assertEquals(resp, mock_resp)
+
