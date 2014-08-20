6fe1bff416d9234fed5edbfb4bd19a8cea18cef0
Tim Cherry <tcherry@livefyre.com>
2014-08-20 16:04:29 -0700

    bad request proto

--- tests/unit_test/test_base_server.py
+++ tests/unit_test/test_base_server.py
@@ -3,11 +3,13 @@ from protobuf_rpc.protos.rpc_pb2 import Request, Response, RPC_ERROR, \
     INVALID_REQUEST_PROTO, METHOD_NOT_FOUND, BAD_REQUEST_PROTO
 from protobuf_rpc.base_server import ProtoBufRPCServer
 from TestService_pb2 import TestService
+from TestService_pb2 import Request as TestRequest
+from TestService_pb2 import Response as TestResponse
 
 class TestServer(TestService):
     def Query(self, controller, request, done):
         assert request.query == "PING"
-        response = Response()
+        response = TestResponse()
         response.response = "PONG"
         done.run(response)
 
@@ -15,7 +17,7 @@ class TestProtoBufRPCServer(unittest.TestCase):
 
     def setUp(self):
         self.pb_server = ProtoBufRPCServer()
-        self.pb_server.service =  TestServer()
+        self.pb_server.service = TestServer()
 
     def test_bad_outer(self):
         bad_request = "abc"
@@ -37,4 +39,15 @@ class TestProtoBufRPCServer(unittest.TestCase):
         bad_request.request_proto =  "Foobar"
         response = self.pb_server.handle(bad_request.SerializeToString())
         self.assertEquals(response.error_code, BAD_REQUEST_PROTO)
+
+
+    def test_bad_request_proto(self):
+        bad_request = Request()
+        bad_request.method_name = "Query"
+        bad_request.service_name = "TestService"
+        test_request = TestRequest()
+        test_request.query = "NOTPING"
+        bad_request.request_proto = test_request.SerializeToString()
+        response = self.pb_server.handle(bad_request.SerializeToString())
+        self.assertEquals(response.error_code, RPC_ERROR)
 
