015af44e78c30af1b50f1b3b8742af2d2cc8e4c8
Tim Cherry <tcherry@livefyre.com>
2014-09-04 15:56:41 -0700

    recv timeout

--- protobuf_rpc/channel.py
+++ protobuf_rpc/channel.py
@@ -3,18 +3,19 @@ from protobuf_rpc.pool import ObjectPool
 from protobuf_rpc.connection import ZMQConnection
 
 class ZMQChannel(ProtoBufRPCChannel):
-    def __init__(self, hosts, pool_size=10, max_pool_size=100):
+    def __init__(self, hosts, pool_size=10, max_pool_size=100, recv_timeout=2*1000):
         self.connection_pool = ObjectPool(ZMQConnection,
                                           size=pool_size,
                                           maxsize=max_pool_size,
                                           hosts=hosts)
+        self.recv_timeout = recv_timeout
 
     def send_rpc_request(self, request):
         with self.connection_pool.get() as con:
             error = None
             con.send(request.SerializeToString())
             try:
-                resp = con.recv()
+                resp = con.recv(self.recv_timeout)
             except IOError as ioe:
                 con.close()
                 error = ioe
